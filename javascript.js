21bbf8d317036a84954fb386a84e46032673f830
muhtajrim <muhtajrin972@gmail.com>
2024-10-02 11:14:38 +0700

    Delete javascript.js

--- javascript.js
+++
@@ -1,7 +0,0 @@
-const burger = document.getElementById("burger");
-const navLinks = document.getElementById("navLinks");
-
-burger.addEventListener("click", () => {
-  navLinks.classList.toggle("active");
-  burger.classList.toggle("toggle");
-});
